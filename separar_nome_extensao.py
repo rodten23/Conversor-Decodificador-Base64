0985aa88d47f865ab6f551b633f37432d219bc3a
rodten23 <rodten23@gmail.com>
2024-01-18 19:30:38 -0300

    :recycle: :sparkles: feat: Separação e refatoração de módulo para separar nome da extensão do arquivo a ser convertido em base64.

---
+++ separar_nome_extensao.py
@@ -0,0 +1,10 @@
+import os
+
+def arq_ext(caminho_completo):
+    global nome_arquivo
+    caminho_sem_extensao, extensao = os.path.splitext(caminho_completo)
+    nome_arquivo = caminho_sem_extensao.split('/')[-1]
+    return nome_arquivo, extensao
+
+if(__name__ == '__main__'):
+    arq_ext()
