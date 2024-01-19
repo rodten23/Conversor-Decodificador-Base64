a34c0289ffdf3b26c6f258881a18b0674e95a9e1
rodten23 <rodten23@gmail.com>
2024-01-19 15:56:10 -0300

    :bug: fix: Correção para retornar extensão no módulo separar_nome_extensão.

--- separar_nome_extensao.py
+++ separar_nome_extensao.py
@@ -1,7 +1,7 @@
 import os
 
 def arq_ext(caminho_completo):
-    global nome_arquivo
+    global nome_arquivo, extensao
     caminho_sem_extensao, extensao = os.path.splitext(caminho_completo)
     nome_arquivo = caminho_sem_extensao.split('/')[-1]
     return nome_arquivo, extensao
