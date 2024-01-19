1a6c0d71bdbf519d45c91c1c5ebe439397477981
rodten23 <rodten23@gmail.com>
2024-01-19 18:47:24 -0300

    :recycle: refactor: Refatoração do decodificador e inclusão da chamado para função base64_nome_ext.

--- decodificador-base64.py
+++ decodificador-base64.py
@@ -1,15 +1,17 @@
+import separar_nome_extensao
 import base64
 
-saida = './saidas/imagem_decodificada3.webp'
+txt_base64 = './saidas/vida-simples-significativa-rotina.jpg.base64.txt'
 
-arquivo_base64 = './saidas/codificado3.txt'
+nome_base64, extensao_original = separar_nome_extensao.base64_nome_ext(txt_base64)
 
-with open(arquivo_base64, 'rt') as file_codicado:
-    data = file_codicado.read()
+saida_descodificada = './saidas/' + nome_base64 + '.' + extensao_original
 
-encoded = data.encode('utf-8')
+with open(txt_base64, 'rt') as txt_descodificar:
+    dados = txt_descodificar.read()
+    codigo_base64 = dados.encode('utf-8')
 
-with open(saida, 'wb') as file:
-    saida_decodificada = base64.decodebytes(encoded)
-    file.write(saida_decodificada)
+with open(saida_descodificada, 'wb') as arquivo_recriado:
+    binario_recriado = base64.decodebytes(codigo_base64)
+    arquivo_recriado.write(binario_recriado)
     
