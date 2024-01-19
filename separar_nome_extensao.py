c03cb251085e6b64d3865fc721c651302f0678ad
rodten23 <rodten23@gmail.com>
2024-01-19 17:58:19 -0300

    :recycle: :sparkles: feat: Refatoração no módulo separar_nome_extensao e inclusão de função para separar nome e extensão do arquivo base64.

--- separar_nome_extensao.py
+++ separar_nome_extensao.py
@@ -1,10 +1,17 @@
 import os
 
-def arq_ext(caminho_completo):
+def arq_nome_ext(caminho_completo):
     global nome_arquivo, extensao
     caminho_sem_extensao, extensao = os.path.splitext(caminho_completo)
     nome_arquivo = caminho_sem_extensao.split('/')[-1]
     return nome_arquivo, extensao
 
+def base64_nome_ext(caminho_completo):
+    global nome_base64, extensao_original
+    arquivo_base64 = caminho_completo.split('/')[-1]
+    nome_base64, extensao_original = arquivo_base64.split('.')[0:2]
+    return nome_base64, extensao_original
+
 if(__name__ == '__main__'):
-    arq_ext()
+    arq_nome_ext()
+    base64_nome_ext()
